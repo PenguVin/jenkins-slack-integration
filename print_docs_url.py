13bbc84d255352323252ee40a826562f9a69d8b1
Viransh Bhardwaj <viransh.bhardwaj@cloudkeeper.com>
2025-06-26 14:11:27 +0530

    general commit

--- print_docs_url.py
+++ print_docs_url.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+import random
 
-print("https://docs.google.com/document/d/1ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop/edit")
+x=random.randint(1, 10)
+y=random.randint(1, 10)
+if x==y:
+    print("https://docs.google.com/document/d/1ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnop/edit")
+else:
+    print("No matching documents found.")
